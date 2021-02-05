f5a971f88ef7956e1eb38fe92870478bb43b0f02
eaxr <61225077+eaxr@users.noreply.github.com>
2021-02-05 13:55:11 +0300

    add check for items
    Added check for items that do not receive data. If the item is turned off, then it is turned on and checked. If no data has been received, it will be disabled.

--- zabbix_vmware.py
+++ zabbix_vmware.py
@@ -7,6 +7,7 @@ import os.path
 import json
 import requests
 import logging
+import time
 
 import zabbix_vmware_settings
 
@@ -24,14 +25,29 @@ class ZabbixAPI:
         payload = {"jsonrpc": "2.0", "method": method, "params": {"user": api_user,"password": api_pass}, "id": 1}
         return self.postReq(payload)
 
-    def getHost(self):
+    def getHost(self, name):
         method = "template.get"
-        payload = {"jsonrpc": "2.0", "method": method, "params": { "output": "extend", "selectHosts": "extend", "filter": { "host": [ "Template VM VMware Guest" ] } }, "auth": self.token['result'], "id": 1}
+        payload = {"jsonrpc": "2.0", "method": method, "params": { "output": "extend", "selectHosts": "extend", "filter": { "host": [ name ] } }, "auth": self.token['result'], "id": 1}
         return self.postReq(payload)
 
     def getItem(self, hostid, key, name):
         method = "item.get"
-        payload = {"jsonrpc": "2.0", "method": method, "params": { "output": ["itemid", "name", "lastvalue", "state"], "hostids": hostid, "search": { "key": key }, "sortfield": "name", "filter": { "name": [ name ] } }, "auth": self.token['result'], "id": 1}
+        payload = {"jsonrpc": "2.0", "method": method, "params": { "output": ["itemid", "name", "lastvalue", "state", "status"], "hostids": hostid, "search": { "key": key }, "sortfield": "name", "filter": { "name": [ name ] } }, "auth": self.token['result'], "id": 1}
+        return self.postReq(payload)
+
+    def getItemDR(self, hostid):
+        method = "item.get"
+        payload = {"jsonrpc": "2.0", "method": method, "params": { "selectDiscoveryRule": ["itemid", "name"], "hostids": hostid }, "auth": self.token['result'], "id": 1}
+        return self.postReq(payload)
+
+    def getItemByID(self, itemid):
+        method = "item.get"
+        payload = {"jsonrpc": "2.0", "method": method, "params": { "output": ["itemid", "name", "lastvalue", "state", "status"], "itemids": itemid }, "auth": self.token['result'], "id": 1}
+        return self.postReq(payload)
+
+    def getDRule(self, hostid, name):
+        method = "discoveryrule.get"
+        payload = {"jsonrpc": "2.0", "method": method, "params": { "output": ["itemid", "name"], "hostids": hostid, "filter": { "name": name } }, "auth": self.token['result'], "id": 1}
         return self.postReq(payload)
 
     def getItemState(self, hostid):
@@ -39,10 +55,38 @@ class ZabbixAPI:
         payload = {"jsonrpc": "2.0", "method": method, "params": { "output": ["itemid", "name", "status"], "hostids": hostid, "sortfield": "name", "filter": { "state": "1" } }, "auth": self.token['result'], "id": 1}
         return self.postReq(payload)
 
+    def createTask(self, itemid):
+        method = "task.create"
+        payload = {"jsonrpc": "2.0", "method": method, "params": { "type": "6", "request": { "itemid": itemid } }, "auth": self.token['result'], "id": 1}
+        return self.postReq(payload)
+
     def updateItem(self, itemid, status):
         method = "item.update"
         payload = {"jsonrpc": "2.0", "method": method, "params": { "itemid": itemid, "status": status }, "auth": self.token['result'], "id": 1}
         return self.postReq(payload)
+
+    def updateItemDR(self, itemid, status, name, hostid, hostname, visname, zbx_logger):
+        if status == "1":
+            print("Status = Disabled")
+            self.updateItem(itemid, "0")
+            print("Item ID =", itemid, "has been enabled")
+            print('Perform "execute now"')
+            task = self.createTask(itemid)
+            print('Task = ', task)
+            time.sleep(1)
+            obj = self.getItemByID(itemid)
+            if obj['result'][0]['state'] == "1":
+                self.updateItem(itemid, "1")
+                print("Item ID =", itemid, "has been disabled")
+                msg = "Item {} with ID = {} doesn't receive data, Host {} ({}) ID = {}".format(name, itemid, hostname, visname, hostid)
+            elif obj['result'][0]['state'] == "1":
+                print("Item ID =", itemid, "began to receive data")
+                msg = "Item {} with ID = {} began to receive data, Host {} ({}) ID = {}".format(name, itemid, hostname, visname, hostid)
+        elif status == "0":
+            print("Status = Enabled")
+            self.updateItem(itemid, "1")
+            msg = "Item {} with ID = {} has been disabled, Host {} ({}) ID = {}".format(name, itemid, hostname, visname, hostid)
+        zbx_logger.warning(msg)
 
     def updateList(self, search2, status, check, zbx_logger, hostname, visname, hostid):
         for itemNum in range(0, len(search2)):
@@ -64,6 +108,7 @@ def main():
     zbx_api_pass = zabbix_vmware_settings.zbx_api_pass
     zbx_api_url = zabbix_vmware_settings.zbx_api_url
     zbx_log_path = zabbix_vmware_settings.zbx_log_path
+    zbx_drule_dict = zabbix_vmware_settings.zbx_drule_dict
 
     zbx_logger = logging.getLogger()
     zbx_logger.setLevel(logging.WARN)
@@ -73,10 +118,10 @@ def main():
     zbx_logger.addHandler(zbx_logfile)
 
     zb = ZabbixAPI(zbx_api_url, zbx_api_user, zbx_api_pass)
-    result = zb.getHost()
+    result = zb.getHost('Template VM VMware Guest')
     search = result['result'][0]['hosts']
     print("=======================================")
-    #zbx_logger.warning("zabbix_vmware start")
+    zbx_logger.warning("zabbix_vmware start")
     for hostNum in range(0, len(search)):
         hostid = search[hostNum]['hostid']
         print("Host ID =", hostid)
@@ -95,6 +140,30 @@ def main():
             search2 = itemState['result']
             zb.updateList(search2, 0, "1", zbx_logger, hostname, visname, hostid)
         print("\n")
+    print("=======================================")
+
+    for template, drule in zbx_drule_dict.items():
+        result = zb.getHost(template)
+        search = result['result'][0]['hosts']
+
+        for hostNum in range(0, len(search)):
+            hostid = search[hostNum]['hostid']
+            hostname = search[hostNum]['host']
+            visname = search[hostNum]['name']
+
+            realDR = zb.getDRule(hostid, drule)
+            hostDR = zb.getItemDR(hostid)
+
+            for itemNum in range(0, len(hostDR['result'])):
+                if hostDR['result'][itemNum]['discoveryRule']:
+                    if hostDR['result'][itemNum]['discoveryRule']['itemid'] == realDR['result'][0]['itemid'] and hostDR['result'][itemNum]['state'] == "1":
+                        print("Host ID =", hostid)
+                        print("Host name =", hostname)
+                        print("Visible name =", visname)
+                        print("Item ID =", hostDR['result'][itemNum]['itemid'], "Name = ", hostDR['result'][itemNum]['name'])
+                        print("State = ", hostDR['result'][itemNum]['state'], "Error = ", hostDR['result'][itemNum]['error'])
+                        zb.updateItemDR(hostDR['result'][itemNum]['itemid'], hostDR['result'][itemNum]['status'], hostDR['result'][itemNum]['name'], hostid, hostname, visname, zbx_logger)
+                        print("\r")
     print("=======================================")
     #zbx_logger.warning("zabbix_vmware finish")
 
