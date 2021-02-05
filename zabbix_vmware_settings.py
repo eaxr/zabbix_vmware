39308a887df68de71422cc6fe5559209545c6aa4
eaxr <61225077+eaxr@users.noreply.github.com>
2021-02-05 13:57:43 +0300

    add selection for template and discovery rule
    By adding the template name and detection rules to the dictionary, it is possible to check the items created by this template.

--- zabbix_vmware_settings.py
+++ zabbix_vmware_settings.py
@@ -5,3 +5,8 @@ zbx_api_user = 'zabbix_api'
 zbx_api_pass = 'zabbix'
 zbx_api_url  = 'https://example.ru/zabbix/api_jsonrpc.php'
 zbx_log_path = '/var/log/zabbix_vmware.log'
+
+zbx_drule_dict = {
+    "Template VM VMware Guest": "Network device discovery",
+    "Template VM VMware Hypervisor": "Datastore discovery"
+}
