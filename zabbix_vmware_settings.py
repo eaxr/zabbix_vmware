c8a1b01874b7d462aca3a96d6693b4dfd619f07c
eaxr <61225077+eaxr@users.noreply.github.com>
2021-03-11 16:50:23 +0300

    add time variable for main loop cycle

--- zabbix_vmware_settings.py
+++ zabbix_vmware_settings.py
@@ -5,6 +5,7 @@ zbx_api_user = 'zabbix_api'
 zbx_api_pass = 'zabbix'
 zbx_api_url  = 'https://example.ru/zabbix/api_jsonrpc.php'
 zbx_log_path = '/var/log/zabbix_vmware.log'
+zbx_wait_time = 900
 
 zbx_drule_dict = {
     "Template VM VMware Guest": "Network device discovery",
