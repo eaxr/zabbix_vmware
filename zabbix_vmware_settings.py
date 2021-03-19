343c82514af70f43214b578153071430058e37cb
eaxr <61225077+eaxr@users.noreply.github.com>
2021-03-19 12:12:15 +0300

    change names
    changing the names of detection rules and templates that are used in the version 5.2

--- zabbix_vmware_settings.py
+++ zabbix_vmware_settings.py
@@ -5,9 +5,12 @@ zbx_api_user = 'zabbix_api'
 zbx_api_pass = 'zabbix'
 zbx_api_url  = 'https://example.ru/zabbix/api_jsonrpc.php'
 zbx_log_path = '/var/log/zabbix_vmware.log'
-zbx_wait_time = 900
+zbx_wait_time = 3600
+zbx_tempname_base = 'VMware Guest'
+zbx_tempname_hypervisor = 'VMware Hypervisor'
 
 zbx_drule_dict = {
-    "Template VM VMware Guest": "Network device discovery",
-    "Template VM VMware Hypervisor": "Datastore discovery"
+    "VMware Guest": "Network device discovery",
+    "VMware Hypervisor": "Datastore discovery",
+    "VMware": "Discover VMware datastores"
 }
